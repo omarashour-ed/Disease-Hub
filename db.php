c8b2d3c2a7a508a5ca42599e4dad178d2e90f953
xxoyxx <125386476+xxoyxx@users.noreply.github.com>
2023-02-14 22:51:37 +0000

    Login

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php
+    $con = mysqli_connect("localhost","yangedin_yangedin","oy2346107725","yangedin_Login");
+    // Check connection
+    if (mysqli_connect_errno()){
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+?>
